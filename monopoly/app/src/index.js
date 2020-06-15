5c7c5c74eba83c3a254882f4e2a473d91a5f0f3a
cyy25 <yangcy@sjtu.edu.cn>
2020-06-15 17:49:00 +0800

    add join room

--- monopoly/app/src/index.js
+++ monopoly/app/src/index.js
@@ -6,87 +6,148 @@ import monopolyArtifact from "../../build/contracts/Monopoly.json";
 let roomId = 0
 
 const App = {
-  web3: null,
-  account: null,
-  monopoly: null,
+    web3: null,
+    account: null,
+    monopoly: null,
 
-  start: async function() {
-    const { web3 } = this;
+    start: async function () {
+        const {web3} = this;
 
-    try {
-      // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = monopolyArtifact.networks[networkId];
-      this.monopoly = new web3.eth.Contract(
-          monopolyArtifact.abi,
-          deployedNetwork.address,
-      );
-      console.log(this.monopoly)
-      // get accounts
-      const accounts = await web3.eth.getAccounts();
-      this.account = accounts[0];
+        try {
+            // get contract instance
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = monopolyArtifact.networks[networkId];
+            this.monopoly = new web3.eth.Contract(
+                monopolyArtifact.abi,
+                deployedNetwork.address,
+            );
 
-      // this.refreshBalance();
-    } catch (error) {
-      console.error("Could not connect to contract or chain.");
+            console.log(this.monopoly)
+            // get accounts
+            const accounts = await web3.eth.getAccounts();
+            this.account = accounts[0];
+
+        } catch (error) {
+            console.error("Could not connect to contract or chain.");
+        }
+    },
+
+
+    setStatus: function (message) {
+        const status = document.getElementById("status");
+        status.innerHTML = message;
+    },
+
+    createRoom: async function () {
+        let self = this;
+        roomId = Math.floor(Math.random() * 900000 + 100000)
+        sessionStorage.setItem('roomId', roomId)
+        document.getElementById('roomid').value = roomId;
+        const {createRoom} = this.monopoly.methods;
+
+        await createRoom(roomId).send({from: this.account}).then(async function (re) {
+            await self.JumptoRoom();
+            self.setStatus('Create room success');
+            console.log('createRoom -- roomId=' + roomId);
+        }).catch(function (e) {
+            console.log('Failed when creating room-' + roomId)
+            console.log(e);
+        });
+    },
+
+
+    joinRoom: async function () {
+        let self = this ;
+        if (document.getElementById('roomid').value) {
+            roomId = parseInt(document.getElementById('roomid').value)
+        }
+        sessionStorage.setItem('roomId', roomId);
+        if (roomId && roomId > 100000 && roomId < 1000000) {
+            const {joinRoom} = this.monopoly.methods;
+            await joinRoom(roomId).send({from: this.account}).then(function (re) {
+                self.JumptoRoom();
+                self.setStatus('Join room success');
+                console.log('joinRoom -- roomId=' + roomId);
+            }).catch(function (e) {
+                console.log('Failed when joining room-' + roomId)
+                console.log(e);
+            });
+
+        } else {
+            self.setStatus('房间号需为6位数字')
+        }
+    },
+
+    JumptoRoom: async function () {
+        document.getElementById('body').innerHTML = "\n" +
+            "<h1>Monopoly</h1>\n" +
+            "<div><b>Room :</b><i id=\"roomid\">000000</i></div>\n" +
+            "\n" +
+            "<div>\n" +
+            "<div id=\"player_info\" style='float: left;width: 300px'>\n" +
+            "<div id=\"p1\">empty</div>\n" +
+            "<div id=\"p2\">empty</div>\n" +
+            "<div id=\"p3\">empty</div>\n" +
+            "<div id=\"p4\">empty</div>\n" +
+            "</div>\n" +
+            "<div id=\"map_info\" style='overflow: hidden'>\n" +
+            "    map\n" +
+            "</div>\n" +
+            "</div>\n" +
+            "</br>\n" +
+            "</br>\n" +
+            "</br>\n" +
+            "<p id=\"status\"> none </p>\n" +
+            "<p>\n" +
+            "    -----------------------------------------------</br>\n" +
+            "    A simple Web page to display.</br>\n" +
+            "    Written by a depressed dog.</br>\n" +
+            "</p>"
+        document.getElementById('roomid').innerHTML = roomId;
+        await this.updatePlayerInfo();
+
+    },
+
+    updatePlayerInfo: async function () {
+        console.log('Updating player information of room: '+roomId)
+        const {getPlayerOne} = this.monopoly.methods;
+        let p_state = await getPlayerOne(roomId).call();
+        document.getElementById('p1').innerHTML =
+            "<div id=\"add1\">"+ p_state[0] + "</div>\n" +
+            "<div id=\"pos1\">"+ p_state[1] + "</div>\n" +
+            "<div id=\"mny1\">"+ p_state[2] + "</div>\n"
+        document.getElementById('p2').innerHTML =
+            "<div id=\"add2\">"+ p_state[3] + "</div>\n" +
+            "<div id=\"pos2\">"+ p_state[4] + "</div>\n" +
+            "<div id=\"mny2\">"+ p_state[5] + "</div>\n"
+        document.getElementById('p3').innerHTML =
+            "<div id=\"add3\">"+ p_state[6] + "</div>\n" +
+            "<div id=\"pos3\">"+ p_state[7] + "</div>\n" +
+            "<div id=\"mny3\">"+ p_state[8] + "</div>\n"
+        document.getElementById('p4').innerHTML =
+            "<div id=\"add4\">"+ p_state[9] + "</div>\n" +
+            "<div id=\"pos4\">"+ p_state[10] + "</div>\n" +
+            "<div id=\"mny4\">"+ p_state[11] + "</div>\n"
     }
-  },
 
-  refreshBalance: async function() {
-    const { getBalance } = this.monopoly.methods;
-    const balance = await getBalance(this.account).call();
-
-    const balanceElement = document.getElementsByClassName("balance")[0];
-    balanceElement.innerHTML = balance;
-  },
-
-  sendCoin: async function() {
-    const amount = parseInt(document.getElementById("amount").value);
-    const receiver = document.getElementById("receiver").value;
-
-    this.setStatus("Initiating transaction... (please wait)");
-
-    const { sendCoin } = this.monopoly.methods;
-    await sendCoin(receiver, amount).send({ from: this.account });
-
-    this.setStatus("Transaction complete!");
-    this.refreshBalance();
-  },
-
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
-  },
-
-  createRoom: async function () {
-    roomId = Math.floor(Math.random() * 900000 + 100000)
-    sessionStorage.setItem('roomId', roomId)
-    document.getElementById('roomid').value = roomId
-    const { createRoom } = this.monopoly.methods;
-    await createRoom(roomId).send({ from: this.account });
-
-    self.setStatus('Create room success')
-    console.log('createRoom -- roomId=' + roomId)
-
-  },
 };
 
 window.App = App;
 
-window.addEventListener("load", function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    window.ethereum.enable(); // get permission to access accounts
-  } else {
-    console.warn(
-        "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
-    );
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(
-        new Web3.providers.HttpProvider("http://127.0.0.1:8545"),
-    );
-  }
+window.addEventListener("load", function () {
+    if (window.ethereum) {
+        // use MetaMask's provider
+        App.web3 = new Web3(window.ethereum);
+        window.ethereum.enable(); // get permission to access accounts
+    } else {
+        console.warn(
+            "No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",
+        );
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        App.web3 = new Web3(
+            new Web3.providers.HttpProvider("http://127.0.0.1:9545"),
+        );
+    }
 
-  App.start();
+    App.start();
 });
--- monopoly/contracts/Monopoly.sol
+++ monopoly/contracts/Monopoly.sol
@@ -10,16 +10,16 @@ contract Monopoly {
 
     struct Player {
         address id;
-        uint position;
-        uint money;
+        uint8 position;
+        uint32 money;
     }
     // price = base_price * (level+1) 0<=level<3 if level==3 cannot buy
-    // tax = base_price * (level+1) * (price_tax_rate + up_rate * level)
+    // tax = base_price * (level+1) * (price_tax_rate + up_rate/10 * level)
     struct Grid {
     uint8 grid_type; //0-personal 1-start 2-event
     address belong_to;
     uint8 level;
-    ufixed8x2 up_rate;
+    uint8 up_rate;
     uint32 base_price;
     }
 
@@ -40,60 +40,33 @@ contract Monopoly {
     event OneStep(uint8 x, uint8 y, uint8 playerTurn);
     event GameStart();
     event GameOver(uint8 winner);
+    event PlayerChange();
 
     function createRoom(uint32 _roomId) public payable {
         require(_roomId > 0, "room id must greater than zero");
         require(rooms[_roomId].players[0].id == address(0x0), "this room is not empty");
         rooms[_roomId].players[0].id = msg.sender;
-        rooms[_roomId].players[0].money = msg.value;
         rooms[_roomId].player_num = 1;
     }
 
     //加入游戏
     function joinRoom(uint32 _roomId) public payable{
         require(_roomId > 0);
-        require(rooms[_roomId].player_num == 0 , "this room is empty");
-        require(rooms[_roomId].player_num == 4, "this room is full");
+        require(rooms[_roomId].player_num != 0 , "this room is empty");
+        require(rooms[_roomId].player_num != 4, "this room is full");
 
-        rooms[_roomId].players[1].id = msg.sender;
-        rooms[_roomId].players[1].money = msg.value;
-        rooms[_roomId].playStatus = GameStatus.playing;
-        rooms[_roomId].playerTurn = 1;
-        emit GameStart();
+        rooms[_roomId].players[rooms[_roomId].player_num].id = msg.sender;
+        // rooms[_roomId].playStatus = GameStatus.playing;
+        rooms[_roomId].player_num = rooms[_roomId].player_num + 1;
+        emit PlayerChange();
     }
 
-
-    function getMyMoney(uint32 _roomId) payable public  {
-        require(rooms[_roomId].playStatus == GameStatus.over || rooms[_roomId].playStatus == GameStatus.start, "The game is playing");
-        require(rooms[_roomId].winner > 0);
-        Player memory player = rooms[_roomId].players[rooms[_roomId].winner - 1];
-        require(player.id == msg.sender);
-        uint senderMoney = player.money;
-        require(senderMoney > 0, "You have got your money");
-        assert(address(this).balance >= senderMoney);
-        address(msg.sender).transfer(senderMoney);
+    function getRoomInfo(uint32 _roomId) public view returns(address p1_add, uint8 p1_pos, uint32 p1_mny,address p2_add, uint8 p2_pos, uint32 p2_mny, address p3_add, uint8 p3_pos, uint32 p3_mny, address p4_add, uint8 p4_pos, uint32 p4_mny){
+        require(_roomId > 0, "room id must greater than zero");
+        Room memory now_room = rooms[_roomId];
+        return (now_room.players[0].id, now_room.players[0].position, now_room.players[0].money, now_room.players[1].id, now_room.players[1].position, now_room.players[1].money, now_room.players[2].id, now_room.players[2].position, now_room.players[2].money, now_room.players[3].id, now_room.players[3].position, now_room.players[3].money);
     }
 
 
 
-    //标记胜出者
-    function winGame(uint8 _winner, uint32 _roomId) private {
-        require(0 <= _winner && _winner <= 2, "invalid winner state");
-        rooms[_roomId].winner = _winner;
-        // 游戏结束
-        if (rooms[_roomId].winner != 0) {
-            rooms[_roomId].playStatus = GameStatus.over;
-            rooms[_roomId].playerTurn = 0;
-            if (rooms[_roomId].winner == 1) {
-                assert(rooms[_roomId].players[1].money >= BET_MONEY);
-                rooms[_roomId].players[1].money -= BET_MONEY;
-                rooms[_roomId].players[0].money += BET_MONEY;
-            }else {
-                assert(rooms[_roomId].players[0].money >= BET_MONEY);
-                rooms[_roomId].players[0].money -= BET_MONEY;
-                rooms[_roomId].players[1].money += BET_MONEY;
-            }
-            emit GameOver(rooms[_roomId].winner);
-        }
-    }
 }
