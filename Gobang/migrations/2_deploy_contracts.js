3bb40b0dfd323f62f8d9441c21d4e0ba4fbbaab3
cyy25 <yangcy@sjtu.edu.cn>
2020-06-27 17:16:30 +0800

    delete other projects

--- Gobang/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Gobang = artifacts.require('./Gobang.sol')
-
-module.exports = function (deployer) {
-  // deployer.deploy(ConvertLib)
-  // deployer.link(ConvertLib, MetaCoin)
-  deployer.deploy(Gobang)
-}
--- Solidity_demo/truffle_helloworld/contracts/math.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.21;
-
-contract Math{
-    function addFunc(uint a, uint b) pure public returns (uint){
-        return a + b;
-    }
-}
